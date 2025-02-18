33d41a7f49c9606b172c97fc8d4c3c0a9c8ad312
YashWadhvani <yashwadhvani123@gmail.com>
2025-02-18 21:56:35 +0530

    Updated Bin Schema

--- models/Bin.js
+++ models/Bin.js
@@ -1,19 +1,21 @@
-// src/models/Bin.js
 const mongoose = require("mongoose");
 
+const readingSchema = new mongoose.Schema({
+  timestamp: { type: Date, required: true },
+  value: { type: Number, required: true },
+});
+
 const binSchema = new mongoose.Schema({
-  binId: { type: String, required: true },
-  weight: { type: Number, required: true },
-  gas_concentration: { type: String, required: true },
-  adc_value: { type: Number, required: true },
-  distance: { type: Number, required: true },
+  binId: { type: String, required: true, unique: true },
+  weight: [readingSchema],  // Stores weight readings over time
+  gas_concentration: [readingSchema],  // Stores gas concentration readings
+  distance: [readingSchema],  // Stores distance readings
+  
   location: {
     latitude: { type: Number, required: true },
     longitude: { type: Number, required: true },
   },
-  timestamp: { type: String, required: true },
 });
 
 const Bin = mongoose.model("Bin", binSchema);
-
 module.exports = Bin;
